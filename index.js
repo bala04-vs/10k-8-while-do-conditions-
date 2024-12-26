bc7627b9c8751a073505f395ba802453ac41f66a
bala04-vs <venkatposina29@gmail.com>
2024-12-26 23:03:50 +0530

    add

---
+++ index.js
@@ -0,0 +1,332 @@
+// what will thw following code  output
+
+// let x=5;
+// while(x<3){
+//     console.log(x);
+//     x++;
+  
+// }
+
+// answer is no output
+
+// what will be the result of the following code
+
+// let y=0;
+// do{
+//     console.log(y);
+//     y++;
+// }
+// while(y<2)
+
+
+    // answer is 0,1
+
+// 3 answer is c
+
+
+
+// 4
+
+// let count = 5;
+// do{
+//     console.log(count);
+//     count--;
+// }
+// while(count>0);
+
+
+// answer is 5, 4 3 2 1
+
+// 5 answer is do while loop
+
+
+// 6 question
+// let z=0;
+// while(z<3){
+
+  
+//     console.log(z);
+//     z++;
+// }
+
+// answer 0,1,2
+
+
+
+// 7 answer c
+
+// 8 question
+
+// let i=0;
+// while(i<5){
+//     console.log(i)
+//     if(i===3) break;
+//     i++;
+// }
+
+// 0,1,2,3
+
+
+
+// 9  answer d
+
+
+// 10 
+
+// let a=0;
+// do{
+//     console.log(a);
+//     a++;
+// }
+// while(a<0)
+
+    // answer a)0
+
+
+
+    // write a code below questions
+    
+//    1
+
+
+    // while loop 
+    // let a=0;
+
+    // while(a<5){
+    //     console.log(a+1)
+    //     a++;
+    // }
+
+
+    // do while 
+
+
+    // let b=0;
+
+    // do{
+    //     console.log(b+1)
+    //     b++;
+    // }
+    // while(b<5)
+
+
+
+
+    // 2
+
+
+    // let a=0;
+
+    // while(a<3)
+    // {
+    //     console.log(a+1);
+    //     if(a===3) break;
+    //     a++;
+    // }
+
+
+    // 3
+
+    // let x=0;
+
+    
+
+    // while(x<10)
+    // {
+    //     x++;
+    //     if(x==5) {
+    //         continue;
+    //     }
+    //     console.log(x)        
+    //     
+    // }
+
+
+    // let x=0;
+
+    // do{
+    //     x++;
+    //     if(x==5){
+    //         continue;
+
+    //     }
+    //     console.log(x)
+    // }
+    // while(x<10)
+
+
+
+
+
+
+    // even numbers
+
+
+    // let y=0;
+
+    // while(y<20){
+    //     if (y%2==0){
+    //         console.log(y)
+    //     }
+    //     y++;
+    // }
+
+
+
+
+    // conditional statements
+
+
+//   greater than 10
+
+    // let x=5;
+    // if(x<10){
+    //     console.log(x,"Number is greater than 10")
+    // }
+   
+
+    // even or odd
+
+    // let z=18;
+
+    // if(z%2==0){
+    //     console.log(z,"is even number");
+
+    // }
+
+    // else{
+    //     console.log(z,"is odd number")
+    // }
+
+
+
+    // caluculate the grade
+
+
+    // let s=82;
+
+
+    // if(s>90){
+    //     console.log("A grade")
+    
+    // }
+    // else if(s>80&&s<89){
+
+    //     console.log("B grade")
+
+        
+        
+    // }
+
+    // else if(s>70&&s<79)
+    // {
+    //     console.log("C grade")
+    // }
+
+    // else if(s<=70){
+    //     console.log("F grade")
+    // }
+
+
+
+//   4 positive number
+
+    // let z=12;
+    // if(z>0){
+    //     if(z%2==0){
+    //         console.log(z,"even number")
+    //     }
+    //     else{
+    //         console.log(z,"odd number")
+    //     }
+
+    //     console.log("positive number")
+    // }
+
+
+
+
+    // 5 rewriting age   
+
+
+    // let age=20;
+
+
+    // if(age>=18){
+    //     console.log("Adult")
+    // }
+
+    // else{
+    //     console.log("Minor")
+    // }
+
+
+
+
+    // 6 range
+
+
+
+    // let h=10;
+    // if(h>10&&h<20){
+    //     console.log("In range")
+    // }
+    // else{
+
+    //     console.log("out of range")
+
+
+    // }
+
+
+    //  8 answer
+
+
+    //   let h=20;
+    // if(h>10&&h<20||h==30){
+    //     console.log("condition met")
+    // }
+    // else{
+
+    //     console.log("out of range")
+
+
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
